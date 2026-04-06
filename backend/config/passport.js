39c25da46bb0219c259f828a12cc33dacaebd08c
Shourya Upadhyay <upadhyayshourya352@gmail.com>
2026-04-07 00:41:37 +0530

    auth

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -7,7 +7,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: "https://placepro-backend.onrender.com/api/auth/google/callback",
       proxy: true,
     },
     async (accessToken, refreshToken, profile, done) => {
